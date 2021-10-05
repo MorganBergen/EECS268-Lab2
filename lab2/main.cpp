1c5cc6330c8d02de0ff862b3069ec61ebd60a948
Morgan Bergen <morganmahabergen@icloud.com>
2021-10-05 11:08:04 -0500

    testing how pointers of class types can be transformed from child class to base class

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -8,8 +8,46 @@
 
 int main(int argc, const char * argv[]) {
 
-    std::cout << "compiled" << std::endl;
+    //of type circle
+    Circle* blue = nullptr;
+    blue = new Circle(10);
+    std::cout << blue -> area() << std::endl;
+    
+    //of type shape
+    Shape* shape1 = nullptr;
+    shape1 = new Circle(10);
+    std::cout << shape1 -> area() << std::endl;
+    std::cout << shape1 -> shapeName() << std::endl;
+    
+    Shape* shape2 = nullptr;
+    shape2 = new Rectangle(10, 20);
+    std::cout << shape2 -> area() << std::endl;
+    std::cout << shape2 -> shapeName() << std::endl;
+    
+    Shape* shape3 = nullptr;
+    shape3 = new Triangle(10, 20);
+    std::cout << shape3 -> area() << std::endl;
+    std::cout << shape3 -> shapeName() << std::endl;
+    
+  
     
     return 0;
 }
 
+
+/*
+ 
+ Shape* array = nullptr;
+ array = new Circle[3];
+ 
+ for (int i = 0; i < 3; i++) {
+     array[i] = *new Circle(i);
+ }
+ 
+ for (int i = 0; i < 3; i++) {
+     std::cout << array[1].area() << std::endl;
+ }
+ 
+
+ 
+ */
