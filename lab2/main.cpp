af983e8cd2baa7a346e885510d0457ed31a06084
Morgan Bergen <morganmahabergen@icloud.com>
2021-10-05 11:34:25 -0500

    Testing how to implement an array of shape pointers. Notes:  This is will soon be a private member variable within a shape container class, however we have not progressed to that stage yet.
    Next Step:
    Create a shape container class which will store the array of shape pointers and hold additional functionality

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -7,47 +7,54 @@
 #include "Triangle.h"
 
 int main(int argc, const char * argv[]) {
-
-    //of type circle
-    Circle* blue = nullptr;
-    blue = new Circle(10);
-    std::cout << blue -> area() << std::endl;
     
-    //of type shape
-    Shape* shape1 = nullptr;
-    shape1 = new Circle(10);
-    std::cout << shape1 -> area() << std::endl;
-    std::cout << shape1 -> shapeName() << std::endl;
+    //an array of shape pointers
+    Shape** arrayOfShapes = nullptr;
     
-    Shape* shape2 = nullptr;
-    shape2 = new Rectangle(10, 20);
-    std::cout << shape2 -> area() << std::endl;
-    std::cout << shape2 -> shapeName() << std::endl;
+    arrayOfShapes = new Shape*[3];
     
-    Shape* shape3 = nullptr;
-    shape3 = new Triangle(10, 20);
-    std::cout << shape3 -> area() << std::endl;
-    std::cout << shape3 -> shapeName() << std::endl;
+    arrayOfShapes[0] = nullptr;
+    arrayOfShapes[1] = nullptr;
+    arrayOfShapes[2] = nullptr;
     
-  
+    for (int i = 0; i < 3; i++) {
+        std::cout << arrayOfShapes[i] << std::endl;
+    }
+    
+    arrayOfShapes[0] = new Circle(10);
+    arrayOfShapes[1] = new Rectangle(10, 20);
+    arrayOfShapes[2] = new Triangle(10, 20);
+    
+    for (int i = 0; i < 3; i++) {
+        std::cout << arrayOfShapes[i] -> shapeName() << std::endl;
+    }
     
     return 0;
 }
 
 
 /*
+ //of type circle
+ Circle* blue = nullptr;
+ blue = new Circle(10);
+ std::cout << blue -> area() << std::endl;
  
- Shape* array = nullptr;
- array = new Circle[3];
+ //of type shape
+ Shape* shape1 = nullptr;
+ shape1 = new Circle(10);
+ std::cout << shape1 -> area() << std::endl;
+ std::cout << shape1 -> shapeName() << std::endl;
  
- for (int i = 0; i < 3; i++) {
-     array[i] = *new Circle(i);
- }
+ Shape* shape2 = nullptr;
+ shape2 = new Rectangle(10, 20);
+ std::cout << shape2 -> area() << std::endl;
+ std::cout << shape2 -> shapeName() << std::endl;
  
- for (int i = 0; i < 3; i++) {
-     std::cout << array[1].area() << std::endl;
- }
+ Shape* shape3 = nullptr;
+ shape3 = new Triangle(10, 20);
+ std::cout << shape3 -> area() << std::endl;
+ std::cout << shape3 -> shapeName() << std::endl;
  
-
+ 
  
  */
