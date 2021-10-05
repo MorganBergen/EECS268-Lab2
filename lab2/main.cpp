09cd8f2e58e5e8ea6b6a6137b4d6bbdc60e378a4
Morgan Bergen <morganmahabergen@icloud.com>
2021-10-05 09:25:28 -0500

    testing member functions of rectangle class

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,28 +1,44 @@
 //  main.cpp
 
-#incbcY9K>
+#include <iostream>
 #include "Shape.h"
 #include "Circle.h"
 #include "Rectangle.h"
 
 int main(int argc, const char * argv[]) {
     
-    double radius;
     
-    std::cout << "please enter radius: ";
-    std::cin >> radius;
+    //testing rectangle class
+    double height;
+    double width;
+    std::cout << "please enter width: ";
+    std::cin >> width;
+    std::cout << "please enter height: ";
+    std::cin >> height;
     
-    Circle* tempCir = nullptr;
+    Rectangle* tempRec = nullptr;
     
-    tempCir = new Circle();
-    tempCir -> setRadius(radius);
-    tempCir -> shapeName();
-    tempCir -> area();
+    tempRec = new Rectangle(height, width);
     
-    std::cout << "cirlce name: " << tempCir -> shapeName() << std::endl;
-    std::cout << "circle area: " << tempCir -> area() << std::endl;
     
     
     return 0;
 }
 
+/*
+ //testing circle class
+ double radius;
+ 
+ std::cout << "please enter radius: ";
+ std::cin >> radius;
+ 
+ Circle* tempCir = nullptr;
+ 
+ tempCir = new Circle();
+ tempCir -> setRadius(radius);
+ tempCir -> shapeName();
+ tempCir -> area();
+ 
+ std::cout << "cirlce name: " << tempCir -> shapeName() << std::endl;
+ std::cout << "circle area: " << tempCir -> area() << std::endl;
+ */
